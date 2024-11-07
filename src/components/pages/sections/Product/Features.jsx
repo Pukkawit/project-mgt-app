a817813df2c0cdada5170c2ec3280e37077cb1de
Witty Umosung <64681844+Pukkawit@users.noreply.github.com>
2024-11-07 22:54:23 +0100

    final base configuration structure

--- src/components/pages/sections/Product/Features.jsx
+++ src/components/pages/sections/Product/Features.jsx
@@ -7,9 +7,9 @@ const Features = () => {
       <h2 className="w-96 text-center heading2  text-darkColors-grey mb-[50px] ">
         Your Best Product Management Platform
       </h2>
-      <div className="flex items-center justify-between gap-x-14">
+      <div className="flex items-center justify-start gap-x-14">
         {features.map((feature, index) => (
-          <div key={index} className="flex justify-start gap-4">
+          <div key={index}>
             <Card
               image={feature.img}
               title={feature.title}
